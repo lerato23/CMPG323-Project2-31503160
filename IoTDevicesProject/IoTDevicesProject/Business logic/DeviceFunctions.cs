e4552d5ffa5cf804c32c68f72bd46bc91013b4b3
Lerato <leratomshengu88@gmail.com>
2022-09-06 21:58:19 +0200

    my third commit

---
+++ IoTDevicesProject/IoTDevicesProject/Business logic/DeviceFunctions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoTDevicesProject.Business_logic
+{
+    public class DeviceFunctions
+    {
+    }
+}
